114a6d5d95af99d198e1822dff00a2d4d102ec7d
Maxim <hristivi8383@gmail.com>
2024-03-14 17:06:21 +0200

    digitalocean db

--- db.js
+++ db.js
@@ -1,5 +1,14 @@
-var pg = require('pg');
-var pool = new pg.Client("postgres://ecsyxvrg:ZgZvXoUCZBJocL9IWuZ89BfgLlMMf6tp@manny.db.elephantsql.com/ecsyxvrg");
+// var pg = require('pg');
+// var pool = new pg.Client("postgres://ecsyxvrg:ZgZvXoUCZBJocL9IWuZ89BfgLlMMf6tp@manny.db.elephantsql.com/ecsyxvrg");
+
+import { Pool } from 'pg'
+var pool = new Pool({
+  host: 'app-19dd1e3d-30eb-46bc-a7a6-731ea9452157-do-user-16045736-0.c.db.ondigitalocean.com',
+  database: 'db',
+  user: 'db',
+  password: 'AVNS_efZ0wA1Zlh1sNrJ5q87',
+  port: 25060,
+})
 
 pool.connect(function(err) {
     if(err) {
